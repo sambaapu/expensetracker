a71cfaaf17596a6546d39f225209818c8b54aa61
Sambit Senapati <sambaapu@gmail.com>
2024-04-10 14:46:58 -0400

    fixed joining condition error

--- src/main/java/com/expense/expensetracker/model/Expense.java
+++ src/main/java/com/expense/expensetracker/model/Expense.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -22,8 +23,10 @@ public class Expense {
     private Instant date;
 
     @ManyToOne
+    @JoinColumn(name="userid")
     private User user;
 
     @ManyToOne
+    @JoinColumn(name="categoryid")
     private Category category;
 }
