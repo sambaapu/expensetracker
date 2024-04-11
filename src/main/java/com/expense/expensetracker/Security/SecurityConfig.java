36f64cb4dfbbcc990e5762cb474621a389b86604
Sambit Senapati <sambaapu@gmail.com>
2024-04-11 02:42:31 -0400

    minor change

--- src/main/java/com/expense/expensetracker/Security/SecurityConfig.java
+++ src/main/java/com/expense/expensetracker/Security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.expense.expensetracker.Security;
 
-import javax.security.enterprise.credential.Password;
+import jakarta.security.enterprise.credential.Password;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
